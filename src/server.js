ad1e300142133f13b01e49308cad84642cd8d016
technicaldifficulties666 <73500878+technicaldifficulties666@users.noreply.github.com>
2021-01-03 17:33:51 -0800

    lesson 1 complete

--- src/server.js
+++ src/server.js
@@ -19,7 +19,11 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  console.log(req.body)
+  res.send(req.body)
 })
 
-export const start = () => {}
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('listening on port 3000');
+  })
+}
