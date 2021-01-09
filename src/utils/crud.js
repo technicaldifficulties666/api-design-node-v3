211e670da961a42a1c77774ae42dd85845902794
technicaldifficulties666 <73500878+technicaldifficulties666@users.noreply.github.com>
2021-01-09 14:01:25 -0800

    initial commit lesson 4

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,57 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  try {
+    const item = await model.findById(req.params.id, req.params._id).exec()
+    res.status(200).json({ result: item })
+  } catch (e) {
+    console.error(e)
+    res.status(404).end()
+  }
+}
 
-export const getMany = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  try {
+    const item = await model.find().exec()
+    res.status(200).json({ data: item })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
 
-export const createOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  try {
+    const newItem = await model.create().exec()
+    res.status(201).send({ message: 'Item created: ' + newItem })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
 
-export const updateOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  try {
+    const item = await model
+      .findByIdAndUpdate(req.params.id, {
+        new: true
+      })
+      .lean()
+      .exec()
+    res.status(200).json({ updatedItem: item })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
 
-export const removeOne = model => async (req, res) => {}
+export const removeOne = model => async (req, res) => {
+  try {
+    const item = await model.findByIdAndRemove(req.params.id).exec()
+    res.status(200).send({ itemRemoved: item })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
@@ -15,3 +60,12 @@ export const crudControllers = model => ({
   getOne: getOne(model),
   createOne: createOne(model)
 })
+
+/*
+  get /item  READ
+  get /item/:id READ ONE
+  post /item CREATE
+  put /item/:id UPDATE ONE
+  delete /item/:id DESTROY ONE
+*/
+
